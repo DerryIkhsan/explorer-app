c1a6d05a517e32b875b0a0b0972dcf04ddb3fb31
Derry <derry@192.168.1.4>
2024-11-01 00:16:51 +0700

    init app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+//import createApp from Vue
+import { createApp } from 'vue';
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far)
+dom.watch();
 
-createApp(App).mount('#app')
+//import component App
+import App from './App.vue';
+
+//import config router
+import router from './router'
+
+//create App Vue
+const app = createApp(App);
+
+//gunakan "router" di Vue dengan plugin "use"
+app.use(router);
+
+app
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app');
