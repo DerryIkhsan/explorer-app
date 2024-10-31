dba8ea525a7afff98d66f75c04f3aa4c6db5732b
Derry <derry@192.168.1.4>
2024-11-01 00:50:06 +0700

    update: folder main

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,25 @@
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import api from '../api';
+
+    const folders = ref([]);
+
+    const fetchDataFolders = async () => {
+
+        //fetch data 
+        await api.get('/api/folders')
+
+        .then(response => {
+            folders.value = response.data.data
+        });
+    }
+
+    onMounted(() => {
+        fetchDataFolders();
+    });
+
+</script>
+
 <template>
     <div class="container-fluid">
         <div class="row flex-nowrap">
@@ -8,21 +30,23 @@
                         <span class="fs-5 d-none d-sm-inline">Explorer App</span>
                     </a>
 
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                        id="menu">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
-                                <font-awesome-icon icon="folder-open" /> 
-                                <span class="ms-1 d-none d-sm-inline">Folder 1</span>
-                            </a>
-                        </li>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+                        <span v-if="folders.length == 0">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link align-middle px-0">
+                                    <span class="ms-1 d-none d-sm-inline">Belum Ada Folder</span>
+                                </a>
+                            </li>
+                        </span>
 
-                        <li class="nav-item">
-                            <a href="#" class="nav-link align-middle px-0">
-                                <font-awesome-icon icon="folder-open" />
-                                <span class="ms-1 d-none d-sm-inline">Folder 2</span>
-                            </a>
-                        </li>
+                        <span v-else v-for="(folder, index) in folders" :key="index">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link align-middle px-0">
+                                    <font-awesome-icon icon="folder-open" /> 
+                                    <span class="ms-1 d-none d-sm-inline">{{ folder.folder }}</span>
+                                </a>
+                            </li>
+                        </span>
                     </ul>
                     <hr>
                 </div>
